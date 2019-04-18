283dbb8691ed70cc685966ef5b62a629861a86e5
Tony <tonyli0513@gmail.com>
2019-04-19 02:41:57 +0800

    feature: ytplayer demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -12,7 +11,7 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
 
